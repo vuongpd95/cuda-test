158bc94f8cd94e5af6e8bf17029053b890febfbf
Vuong Pham-Duy <vuongpd95@gmail.com>
2017-12-22 04:59:05 +0700

    make a CUDA Dynamic Programming loop, need further testing

--- shfl_active/shfl_active.cu
+++ shfl_active/shfl_active.cu
@@ -22,6 +22,7 @@
 #include <inttypes.h>
 
 #define WARP 32
+#define LANE 2
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
 {
@@ -38,18 +39,41 @@ typedef struct {
 	int32_t h, e;
 } eh_t;
 
-extern __shared__ eh_t s_qp[];
-
+__device__
+bool check_active(eh_t data) {
+	if(data.h != -1 && data.e != -1) return true;
+	else return false;
+}
+__device__
+void reset(eh_t *data) {
+	data->h = -1;
+	data->e = -1;
+}
 __global__ 
 void func_0(int qlen, eh_t *d_qp) {
 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 	int lane_id = threadIdx.x % 32;
-	eh_t data;
-	int beg = 0, f = 0;
+	eh_t data_out, data_in;
+	reset(&data_in); reset(&data_out);
+	int beg = 0;
+	if(lane_id == LANE) printf("Lane %d, received: ");
+	if(lane_id == 0) memcpy(s_qp, d_qp, sizeof(int8_t) * (qlen + 1));
 	if(lane_id == 0) {
-		memcpy(s_qp, d_qp, sizeof(int8_t) * (qlen + 1));
+		data_out = s_qp[beg];
+		beg += 1;
 	}
+	__syncthreads();
 	do {
+		if(lane_id == 0) data_in = s_qp[beg];
+		else data_in = __shfl(data_out, lane_id - 1, WARP);
+		if(check_active(data_in)) {
+			if(lane_id == LANE) printf("[%ld, %ld] ", data_in.h, data_in.e);
+			data_out = data_in;
+			reset(&data_in);
+			beg += 1;
+		} else {
+			if(lane_id == LANE) printf("[nothing] ");
+		}
 	} while(beg < qlen);
 }
 
